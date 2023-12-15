bbd02446bebce3f70b74c02e8d818abb5cac64be
Michal Leszczynski <ml@icedev.pl>
2023-12-15 23:19:09 +0100

    fix

--- api/common.js
+++ api/common.js
@@ -12,7 +12,10 @@ const {
     NFCAbortedError,
     NFCOperationError
 } = require("../halo/exceptions");
-const {parsePublicKeys, convertSignature, recoverPublicKey, SECP256k1_ORDER, BJJ_ORDER} = require("../halo/util");
+const {
+    parsePublicKeys, convertSignature, recoverPublicKey, sigToDer,
+    SECP256k1_ORDER, BJJ_ORDER
+} = require("../halo/util");
 
 /**
  * The LibHaLo stable API. Please don't depend on the functions imported from anywhere else
@@ -23,6 +26,7 @@ module.exports = {
     haloParsePublicKeys: parsePublicKeys,
     haloConvertSignature: convertSignature,
     haloRecoverPublicKey: recoverPublicKey,
+    haloSignatureToDer: sigToDer,
 
     SECP256k1_ORDER,
     BJJ_ORDER,
