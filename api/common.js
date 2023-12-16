124b98eab18f2f3f7f2a97ca93ee0d0247abd115
Michał Leszczyński <ml@arx.sh>
2023-12-16 02:52:33 +0100

    HaLo: Always reassemble DER signature to avoid formatting errors (#287)

--- api/common.js
+++ api/common.js
@@ -12,7 +12,10 @@ const {
     NFCAbortedError,
     NFCOperationError
 } = require("../halo/exceptions");
-const {parsePublicKeys, convertSignature, recoverPublicKey} = require("../halo/util");
+const {
+    parsePublicKeys, convertSignature, recoverPublicKey, sigToDer,
+    SECP256k1_ORDER, BJJ_ORDER
+} = require("../halo/util");
 
 /**
  * The LibHaLo stable API. Please don't depend on the functions imported from anywhere else
@@ -23,6 +26,10 @@ module.exports = {
     haloParsePublicKeys: parsePublicKeys,
     haloConvertSignature: convertSignature,
     haloRecoverPublicKey: recoverPublicKey,
+    haloSignatureToDer: sigToDer,
+
+    SECP256k1_ORDER,
+    BJJ_ORDER,
 
     // exceptions
     HaloTagError,
